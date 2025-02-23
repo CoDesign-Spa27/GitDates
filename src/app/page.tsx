60f47cad25edd9b6c887320edd8747f039fa6532
2sandeep7 <ssandeep8696@gmail.com>
2025-02-23 12:42:08 +0530

    added landing and navbar starting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Landing from "@/components/landing";
 import { getServerSession } from "next-auth";
 
 export default async function Home() {
@@ -5,6 +6,10 @@ export default async function Home() {
 
   return (
     <div className="">
+      <Landing />
+      <div className="flex flex-col items-center justify-center"> 
+        <h1>Welcome to the best AI code editor</h1>
+      </div>
     </div>
   );
 }
