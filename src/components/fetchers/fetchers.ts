9ecb859721a9c41dad9bdc27170648da1ec135bc
2sandeep7 <ssandeep8696@gmail.com>
2025-04-13 23:52:14 +0530

    fix:created getmatchpreferences fetcher and better error handling in fethcers

--- src/components/fetchers/fetchers.ts
+++ src/components/fetchers/fetchers.ts
@@ -1,27 +1,63 @@
-import { findMatches, getAllAccounts } from "@/actions/match.action";
+import { findMatches, getAllAccounts, getMatchPreference } from "@/actions/match.action";
 import { toast } from "@/hooks/use-toast";
  
 
-export const findMatchesFetcher = async (email: string) => {
-  const response = await findMatches(email);
-  if (!response?.status) {
-    toast({
-        title: "Failed to load matches",
+export const getMatchPreferencesFetcher = async (email: string) => {
+  try{
+
+    const response = await getMatchPreference(email);
+    if (!response?.status) {
+      toast({
+        title: "Failed to load match preferences",
         variant: "destructive",
+      })
+    }
+    if(response?.status)
+      return response;
+  }catch (error) { 
+    toast({
+      title: "Internal Server Error",
+      variant: "destructive",
     })
   }
-  if(response?.status)
-  return response;
+}
+
+export const findMatchesFetcher = async (email: string) => {
+  try{
+
+    const response = await findMatches(email);
+    if (!response?.status) {
+      toast({
+        title: "Failed to load matches",
+        variant: "destructive",
+      })
+    }
+    if(response?.status)
+      return response;
+  }catch (error) {
+    toast({
+      title: "Internal Server Error",
+      variant: "destructive",
+    })
+  }
 };
 
 export const getAllAccountsFetcher = async () => { 
-  const response = await getAllAccounts();
-  if (!response?.status) {
-    toast({
+  try{
+
+    const response = await getAllAccounts();
+    if (!response?.status) {
+      toast({
         title: "Failed to load accounts",
         variant: "destructive",
+      })
+    }
+    if(response?.status)
+      return response;
+  }catch (error) {
+    toast({
+      title: "Internal Server Error",
+      variant: "destructive",
     })
   }
-  if(response?.status)
-  return response;
 }
