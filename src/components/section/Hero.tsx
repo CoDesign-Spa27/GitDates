41fec52ea5bfca3bbc2704610074c1144f480dee
2sandeep7 <ssandeep8696@gmail.com>
2025-06-15 14:44:50 +0530

    fix:hero page UI

--- src/components/section/Hero.tsx
+++ src/components/section/Hero.tsx
@@ -16,11 +16,17 @@ import {
   Command,
   MessageSquare,
   ChevronDown,
+  ChevronRight,
+  Settings,
 } from "lucide-react";
 import Image from "next/image";
 import { undermark } from "../../../public/assets";
 import { FeatureCard } from "../feature-card";
 import { useRouter } from "next/navigation";
+import { AnimatedGradientText } from "../ui/animated-gradient-text";
+import { cn } from "@/lib/utils";
+import { CTAButton } from "../ui/cta-button";
+ 
 
 const Hero = () => {
   const [isHovered, setIsHovered] = useState(false);
@@ -70,77 +76,9 @@ const Hero = () => {
     <section
       id="home"
       ref={containerRef}
-      className="relative min-h-screen w-full overflow-hidden bg-gradient-to-br from-[#0C0A09]  to-[#0C0A09]"
+      className="relative min-h-screen w-full overflow-hidden bg-cover bg-center bg-[url('/hero-bg-mobile.png')] md:bg-[url('/hero-bg.png')] bg-no-repeat bg-fixed"
     >
-      <motion.div
-        
-        className="absolute inset-0 opacity-10"
-        variants={patternVariants}
-        animate="animate"
-        style={{
-          background:
-            "radial-gradient(circle at 25% 25%, rgba(255, 0, 128, 0.2) 2px, transparent 0), radial-gradient(circle at 75% 75%, rgba(90, 90, 255, 0.2) 2px, transparent 0)",
-          backgroundSize: "60px 60px",
-        }}
-      />
-
-      <div className="absolute inset-0 overflow-hidden">
-        <motion.div
-          className="absolute top-10 left-1/4 w-96 h-96 rounded-full bg-gradient-to-r from-pink-500/30 to-purple-500/30 blur-[120px]"
-          animate={{
-            x: [0, 80, -20, 70, -10, 0],
-            y: [0, -40, 30, -20, 40, 0],
-            scale: [1, 1.2, 0.9, 1.1, 0.8, 1],
-          }}
-          transition={{
-            duration: 15,
-            repeat: Infinity,
-            ease: "easeInOut",
-          }}
-        />
-        <motion.div
-          className="absolute bottom-10 right-1/4 w-[500px] h-[500px] rounded-full bg-gradient-to-r from-blue-500/20 to-cyan-500/20 blur-[120px]"
-          animate={{
-            x: [0, -40, 30, -20, 40, 0],
-            y: [0, 30, -40, 20, -30, 0],
-            scale: [1, 0.8, 1.2, 0.9, 1.1, 1],
-          }}
-          transition={{
-            duration: 18,
-            repeat: Infinity,
-            ease: "easeInOut",
-          }}
-        />
-      </div>
-      <div className="absolute inset-0 overflow-hidden">
-        <motion.div
-          className="absolute bottom-10 -right-1/4 w-96 h-96 rounded-full bg-gradient-to-r from-pink-500/30 to-purple-500/30 blur-[120px]"
-          animate={{
-            x: [0, 80, -20, 70, -10, 0],
-            y: [0, -40, 30, -20, 40, 0],
-            scale: [1, 1.2, 0.9, 1.1, 0.8, 1],
-          }}
-          transition={{
-            duration: 15,
-            repeat: Infinity,
-            ease: "easeInOut",
-          }}
-        />
-        <motion.div
-          className="absolute bottom-0 -right-1/3 w-[500px] h-[500px] rounded-full bg-gradient-to-r from-blue-500/20 to-cyan-500/20 blur-[120px]"
-          animate={{
-            x: [0, -40, 30, -20, 40, 0],
-            y: [0, 30, -40, 20, -30, 0],
-            scale: [1, 0.8, 1.2, 0.9, 1.1, 1],
-          }}
-          transition={{
-            duration: 18,
-            repeat: Infinity,
-            ease: "easeInOut",
-          }}
-        />
-      </div>
-
+ 
       <div className="absolute inset-0 pointer-events-none">
         {codeSnippets.map((snippet, index) => (
           <motion.div
@@ -233,47 +171,44 @@ const Hero = () => {
         className="relative z-10 flex flex-col items-center justify-center min-h-screen px-4 text-center"
         style={{ y: y1, opacity }}
       >
-        <div className="mt-24 mb-8">
-          <motion.div
-            className="flex items-center justify-center mb-6"
-            animate={{
-              scale: [1, 1.05, 1],
-            }}
-            transition={{ duration: 3, repeat: Infinity }}
-          >
-            <motion.div
-              className="relative"
-              animate={{
-                rotate: [0, 360],
-              }}
-              transition={{ duration: 20, repeat: Infinity, ease: "linear" }}
-            >
-              <div className="absolute inset-0 rounded-full bg-gradient-to-r from-pink-500 to-purple-600 blur-md opacity-70"></div>
-              <Github className="relative text-white text-6xl" />
-            </motion.div>
-            <motion.div
-              animate={{
-                scale: [1, 1.3, 1],
-              }}
-              transition={{ duration: 1.5, repeat: Infinity }}
-              className="mx-4 z-10"
-            >
-              <Heart className="text-pink-500 text-5xl drop-shadow-lg" />
-            </motion.div>
-            <motion.div
-              className="relative"
-              animate={{
-                rotate: [0, -360],
-              }}
-              transition={{ duration: 20, repeat: Infinity, ease: "linear" }}
-            >
-              <div className="absolute inset-0 rounded-full bg-gradient-to-r from-blue-500 to-cyan-600 blur-md opacity-70"></div>
-              <Code className="relative text-white text-6xl" />
-            </motion.div>
-          </motion.div>
-
+        <div className="mb-8">
+        <div className="group w-full max-w-max relative mx-auto flex items-center justify-center rounded-full px-4 py-1.5 shadow-[inset_0_-8px_10px_#8fdfff1f] transition-shadow duration-500 ease-out hover:shadow-[inset_0_-5px_10px_#8fdfff3f] ">
+      <span
+        className={cn(
+          "absolute inset-0 block h-full w-full animate-gradient rounded-[inherit] bg-gradient-to-r from-gitdate/50 via-[#9c40ff]/50 to-gitdate/50 bg-[length:300%_100%] p-[1px]",
+        )}
+        style={{
+          WebkitMask:
+            "linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0)",
+          WebkitMaskComposite: "destination-out",
+          mask: "linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0)",
+          maskComposite: "subtract",
+          WebkitClipPath: "padding-box",
+        }}
+      />
+      <motion.div
+        className="px-2"
+        animate={{
+          rotate: 360,
+        }}
+        transition={{
+          duration: 8,
+          repeat: Infinity,
+          ease: "linear",
+        }}
+      >
+        <Settings className="size-6 text-gitdate/50" />
+      </motion.div>
+      <AnimatedGradientText className="text-sm font-medium">
+        Introducing GitDate
+      </AnimatedGradientText>
+      <ChevronRight
+        className="ml-1 size-4 stroke-neutral-500 transition-transform
+ duration-300 ease-in-out group-hover:translate-x-0.5"
+      />
+    </div>
           <motion.h1
-            className="font-riffic text-5xl md:text-7xl lg:text-8xl font-bold mb-8 text-white relative tracking-tight"
+            className="font-riffic text-5xl md:text-7xl lg:text-8xl font-bold mb-8 text-white relative tracking-tight pt-2"
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ delay: 0.3, duration: 0.8 }}
@@ -308,48 +243,8 @@ const Hero = () => {
             <span className="text-pink-400">pair programmer</span> for life.
             <br className="hidden md:block" /> Where code meets chemistry.
           </motion.p>
-
-          <motion.div
-            className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-6    justify-center"
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ delay: 0.9, duration: 0.8 }}
-          >
-            <motion.button
-              onClick={() => navigate.push('/dashboard')}
-              className="group relative px-8 py-4 bg-gradient-to-r from-pink-600 to-purple-600 text-white rounded-full font-semibold text-lg transition-all overflow-hidden shadow-lg hover:shadow-xl"
-              whileHover={{ scale: 1.05, y: -2 }}
-              whileTap={{ scale: 0.95 }}
-              onHoverStart={() => setIsHovered(true)}
-              onHoverEnd={() => setIsHovered(false)}
-            >
-              <motion.span
-                className="absolute inset-0 bg-gradient-to-r from-pink-500 to-purple-500 opacity-0 group-hover:opacity-100 transition-opacity duration-300"
-                initial={{ x: "-100%" }}
-                animate={isHovered ? { x: 0 } : { x: "-100%" }}
-                transition={{ duration: 0.4 }}
-              />
-              <motion.span
-                className="relative flex items-center justify-center gap-3"
-                animate={isHovered ? { x: [0, 5, 0] } : {}}
-                transition={{ duration: 0.3, repeat: isHovered ? 1 : 0 }}
-              >
-                <Github className="w-6 h-6" /> 
-                <span className="tracking-wide">Let's Date</span>
-              </motion.span>
-            </motion.button>
-            {/* <motion.button
-              className="px-6 py-4 bg-transparent border-2 border-white/30 backdrop-blur-sm text-white rounded-full font-semibold text-lg transition-all"
-              whileHover={{
-                scale: 1.05,
-                backgroundColor: "rgba(255,255,255,0.1)",
-                borderColor: "rgba(255,255,255,0.5)",
-              }}
-              whileTap={{ scale: 0.95 }}
-            >
-              Browse Matches
-            </motion.button> */}
-          </motion.div>
+          <CTAButton> Let's Match </CTAButton>
+          
 
           <motion.div
             className="absolute bottom-10 left-0 right-0 flex justify-center"
