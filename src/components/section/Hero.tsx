17b338eebf1269ae0a06697eaf325b126a639630
2sandeep7 <ssandeep8696@gmail.com>
2025-06-17 12:55:50 +0530

    fix: added links to landing page

--- src/components/section/Hero.tsx
+++ src/components/section/Hero.tsx
@@ -1,13 +1,13 @@
-"use client";
+'use client'
 
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from 'react'
 import {
   motion,
   useAnimation,
   useScroll,
   useTransform,
   AnimatePresence,
-} from "motion/react";
+} from 'motion/react'
 import {
   Github,
   Heart,
@@ -18,68 +18,63 @@ import {
   ChevronDown,
   ChevronRight,
   Settings,
-} from "lucide-react";
-import Image from "next/image";
-import { undermark } from "../../../public/assets";
-import { FeatureCard } from "../feature-card";
-import { useRouter } from "next/navigation";
-import { AnimatedGradientText } from "../ui/animated-gradient-text";
-import { cn } from "@/lib/utils";
-import { CTAButton } from "../ui/cta-button";
- 
+} from 'lucide-react'
+import Image from 'next/image'
+import { undermark } from '../../../public/assets'
+import { FeatureCard } from '../feature-card'
+import { useRouter } from 'next/navigation'
+import { AnimatedGradientText } from '../ui/animated-gradient-text'
+import { cn } from '@/lib/utils'
+import { CTAButton } from '../ui/cta-button'
+import Link from 'next/link'
 
 const Hero = () => {
-  const [isHovered, setIsHovered] = useState(false);
-  const { scrollY } = useScroll();
-  const containerRef = useRef(null);
-  const controls = useAnimation();
-  const navigate = useRouter();
- 
-  const y1 = useTransform(scrollY, [0, 500], [0, -100]);
-  const y2 = useTransform(scrollY, [0, 500], [0, -50]);
-  const opacity = useTransform(scrollY, [0, 300], [1, 0]);
+  const [isHovered, setIsHovered] = useState(false)
+  const { scrollY } = useScroll()
+  const containerRef = useRef(null)
+  const controls = useAnimation()
+  const router = useRouter()
+
+  const y1 = useTransform(scrollY, [0, 500], [0, -100])
+  const y2 = useTransform(scrollY, [0, 500], [0, -50])
+  const opacity = useTransform(scrollY, [0, 300], [1, 0])
 
   useEffect(() => {
     controls.start({
       opacity: 1,
       y: 0,
-      transition: { duration: 0.8, ease: "easeOut" },
-    });
-  }, [controls]);
+      transition: { duration: 0.8, ease: 'easeOut' },
+    })
+  }, [controls])
 
   const codeSnippets = [
-    "if (youCode && iCode) { return match(); }",
+    'if (youCode && iCode) { return match(); }',
     "git commit -m 'Found my perfect match'",
-    "function findLove() { return you.merge(me); }",
-    "try { us(); } catch (feelings) { embrace(feelings); }",
-    "while(true) { commit(love); push(affection); }",
-    "for(let i = 0; i < infinity; i++) { loveCoding(with: you); }",
-    "class Relationship extends Perfect { constructor(you, me) {...} }",
-  ];
+    'function findLove() { return you.merge(me); }',
+    'try { us(); } catch (feelings) { embrace(feelings); }',
+    'while(true) { commit(love); push(affection); }',
+    'for(let i = 0; i < infinity; i++) { loveCoding(with: you); }',
+    'class Relationship extends Perfect { constructor(you, me) {...} }',
+  ]
 
- 
   const patternVariants: any = {
     animate: {
-      backgroundPosition: ["0% 0%", "100% 100%"],
+      backgroundPosition: ['0% 0%', '100% 100%'],
       transition: {
         duration: 20,
-        ease: "linear",
+        ease: 'linear',
         repeat: Infinity,
-        repeatType: "reverse",
+        repeatType: 'reverse',
       },
     },
-  };
-
- 
+  }
 
   return (
     <section
       id="home"
       ref={containerRef}
-      className="relative min-h-screen w-full overflow-hidden bg-cover bg-center bg-[url('/hero-bg-mobile.png')] md:bg-[url('/hero-bg.png')] bg-no-repeat bg-fixed"
-    >
- 
-      <div className="absolute inset-0 pointer-events-none">
+      className="relative min-h-screen w-full overflow-hidden bg-[url('/hero-bg-mobile.png')] bg-cover bg-fixed bg-center bg-no-repeat md:bg-[url('/hero-bg.png')]">
+      <div className="pointer-events-none absolute inset-0">
         {codeSnippets.map((snippet, index) => (
           <motion.div
             key={index}
@@ -93,28 +88,27 @@ const Hero = () => {
               x: {
                 duration: 7 + index * 0.5,
                 repeat: Infinity,
-                repeatType: "reverse",
+                repeatType: 'reverse',
               },
               y: {
                 duration: 9 + index * 0.7,
                 repeat: Infinity,
-                repeatType: "reverse",
+                repeatType: 'reverse',
               },
               opacity: { duration: 1 },
             }}
-            className="absolute text-green-400 font-mono text-xs md:text-sm lg:text-base"
+            className="absolute font-mono text-xs text-green-400 md:text-sm lg:text-base"
             style={{
               left: `${5 + index * 14}%`,
               top: `${10 + ((index * 12) % 80)}%`,
               transform: `rotate(${index * 3 - 5}deg)`,
-            }}
-          >
+            }}>
             {snippet}
           </motion.div>
         ))}
       </div>
 
-      <div className="absolute inset-0 pointer-events-none">
+      <div className="pointer-events-none absolute inset-0">
         {[...Array(20)].map((_, index) => (
           <motion.div
             key={`icon-${index}`}
@@ -126,21 +120,21 @@ const Hero = () => {
               rotate: [0, index % 2 === 0 ? 10 : -10, 0],
             }}
             transition={{
-              opacity: { duration: 5, repeat: Infinity, repeatType: "reverse" },
+              opacity: { duration: 5, repeat: Infinity, repeatType: 'reverse' },
               x: {
                 duration: 7 + index * 0.3,
                 repeat: Infinity,
-                repeatType: "reverse",
+                repeatType: 'reverse',
               },
               y: {
                 duration: 8 + index * 0.5,
                 repeat: Infinity,
-                repeatType: "reverse",
+                repeatType: 'reverse',
               },
               rotate: {
                 duration: 10 + index * 0.2,
                 repeat: Infinity,
-                repeatType: "reverse",
+                repeatType: 'reverse',
               },
             }}
             className="absolute text-white/30"
@@ -148,8 +142,7 @@ const Hero = () => {
               left: `${Math.floor(Math.random() * 90)}%`,
               top: `${Math.floor(Math.random() * 90)}%`,
               fontSize: `${Math.floor(Math.random() * 24) + 14}px`,
-            }}
-          >
+            }}>
             {index % 6 === 0 ? (
               <Github />
             ) : index % 6 === 1 ? (
@@ -168,103 +161,93 @@ const Hero = () => {
       </div>
 
       <motion.div
-        className="relative z-10 flex flex-col items-center justify-center min-h-screen px-4 text-center"
-        style={{ y: y1, opacity }}
-      >
+        className="relative z-10 flex min-h-screen flex-col items-center justify-center px-4 text-center"
+        style={{ y: y1, opacity }}>
         <div className="mb-8">
-        <div className="group w-full max-w-max relative mx-auto flex items-center justify-center rounded-full px-4 py-1.5 shadow-[inset_0_-8px_10px_#8fdfff1f] transition-shadow duration-500 ease-out hover:shadow-[inset_0_-5px_10px_#8fdfff3f] ">
-      <span
-        className={cn(
-          "absolute inset-0 block h-full w-full animate-gradient rounded-[inherit] bg-gradient-to-r from-gitdate/50 via-[#9c40ff]/50 to-gitdate/50 bg-[length:300%_100%] p-[1px]",
-        )}
-        style={{
-          WebkitMask:
-            "linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0)",
-          WebkitMaskComposite: "destination-out",
-          mask: "linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0)",
-          maskComposite: "subtract",
-          WebkitClipPath: "padding-box",
-        }}
-      />
-      <motion.div
-        className="px-2"
-        animate={{
-          rotate: 360,
-        }}
-        transition={{
-          duration: 8,
-          repeat: Infinity,
-          ease: "linear",
-        }}
-      >
-        <Settings className="size-6 text-gitdate/50" />
-      </motion.div>
-      <AnimatedGradientText className="text-sm font-medium">
-        Introducing GitDate
-      </AnimatedGradientText>
-      <ChevronRight
-        className="ml-1 size-4 stroke-neutral-500 transition-transform
- duration-300 ease-in-out group-hover:translate-x-0.5"
-      />
-    </div>
+          <div onClick={()=>router.push('/signin')} className="cursor-pointer group relative mx-auto flex w-full max-w-max items-center justify-center rounded-full px-4 py-1.5 shadow-[inset_0_-8px_10px_#8fdfff1f] transition-shadow duration-500 ease-out hover:shadow-[inset_0_-5px_10px_#8fdfff3f]">
+            <span
+              className={cn(
+                'animate-gradient absolute inset-0 block h-full w-full rounded-[inherit] bg-gradient-to-r from-gitdate/50 via-[#9c40ff]/50 to-gitdate/50 bg-[length:300%_100%] p-[1px]'
+              )}
+              style={{
+                WebkitMask:
+                  'linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0)',
+                WebkitMaskComposite: 'destination-out',
+                mask: 'linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0)',
+                maskComposite: 'subtract',
+                WebkitClipPath: 'padding-box',
+              }}
+            />
+            <motion.div
+              className="px-2"
+              animate={{
+                rotate: 360,
+              }}
+              transition={{
+                duration: 8,
+                repeat: Infinity,
+                ease: 'linear',
+              }}>
+              <Settings className="size-6 text-gitdate/50" />
+            </motion.div>
+
+            <AnimatedGradientText className="text-sm font-medium">
+              Introducing GitDates
+            </AnimatedGradientText>
+
+            <ChevronRight className="ml-1 size-4 stroke-neutral-500 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5" />
+          </div>
           <motion.h1
-            className="font-riffic text-5xl md:text-7xl lg:text-8xl font-bold mb-8 text-white relative tracking-tight pt-2"
+            className="relative mb-8 pt-2 font-riffic text-5xl font-bold tracking-tight text-white md:text-7xl lg:text-8xl"
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
-            transition={{ delay: 0.3, duration: 0.8 }}
-          >
-            <span className="text-transparent bg-clip-text bg-gitdate ">
+            transition={{ delay: 0.3, duration: 0.8 }}>
+            <span className="bg-gitdate bg-clip-text text-transparent">
               git
             </span>
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-600 to-white">
+            <span className="bg-gradient-to-r from-purple-600 to-white bg-clip-text text-transparent">
               Connected
             </span>
             <div
-              className="absolute w-full flex justify-center"
-              style={{ top: "100%", left: "0" }}
-            >
+              className="absolute flex w-full justify-center"
+              style={{ top: '100%', left: '0' }}>
               <Image
                 src={undermark}
                 width={300}
                 height={300}
                 alt="undermark"
-                className="w-[200px] md:w-[290px] sm:-mt-24 -mt-16"
+                className="-mt-16 w-[200px] sm:-mt-24 md:w-[290px]"
               />
             </div>
           </motion.h1>
 
           <motion.p
-            className="text-md md:text-xl lg:text-2xl text-gray-300 mb-12 max-w-3xl mx-auto font-light"
+            className="text-md mx-auto mb-12 max-w-3xl font-light text-gray-300 md:text-xl lg:text-2xl"
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
-            transition={{ delay: 0.6, duration: 0.8 }}
-          >
-            Find your perfect{" "}
+            transition={{ delay: 0.6, duration: 0.8 }}>
+            Find your perfect{' '}
             <span className="text-pink-400">pair programmer</span> for life.
             <br className="hidden md:block" /> Where code meets chemistry.
           </motion.p>
-          <CTAButton> Let's Match </CTAButton>
-          
+          <CTAButton onClick={()=>router.push('/signin')} className='text-white'> Let's Match </CTAButton>
 
           <motion.div
             className="absolute bottom-10 left-0 right-0 flex justify-center"
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
-            transition={{ delay: 1.5, duration: 0.8 }}
-          >
+            transition={{ delay: 1.5, duration: 0.8 }}>
             <motion.div
-              className="text-white/60 cursor-pointer flex flex-col items-center"
+              className="flex cursor-pointer flex-col items-center text-white/60"
               animate={{ y: [0, 10, 0] }}
               transition={{ duration: 2, repeat: Infinity }}
-              whileHover={{ color: "#ffffff" }}
-            >
-              <p className="text-sm mb-2">Scroll to discover</p>
+              whileHover={{ color: '#ffffff' }}>
+              <p className="mb-2 text-sm">Scroll to discover</p>
               <ChevronDown />
             </motion.div>
           </motion.div>
         </div>
       </motion.div>
- 
 
       <style jsx global>{`
         @keyframes pulse {
@@ -304,7 +287,7 @@ const Hero = () => {
         }
       `}</style>
     </section>
-  );
-};
+  )
+}
 
-export default Hero;
+export default Hero
--- src/components/section/faq.tsx
+++ src/components/section/faq.tsx
@@ -71,78 +71,80 @@ const FaqItem = ({ faq, index }:any) => {
 
 const FAQSection = () => {
   return (
-    <section id="faq" className="py-20 bg-[#0C0A09]">
-    <div className="relative container mx-auto px-4 md:px-6 lg:px-8">
-        
-          <motion.div
-            className="absolute right-20 left-10 w-44 h-44 rounded-full bg-gradient-to-r from-yellow-500/40 to-green-400/60 blur-[100px]"
-            animate={{
-              x: [0, 50, -20, 40, 0],
-              y: [0, -30, 20, -20, 0],
-              rotate: [0, 20, -50, 50, 0],
-            }}
-            transition={{
-              duration: 10,
-              repeat: Infinity,
-              ease: "linear",
-            }}
-            />
-      
-            <motion.div
-            className="absolute bottom-10 right-10 md:right-20 w-52 h-52 rounded-full bg-gradient-to-r from-yellow-500/40 to-green-400/60  blur-[90px] "
-            animate={{
-                x: [0, 10, -20, 10, 0],
-                y: [0, -10, 20, -20, 0],
-                rotate: [0, 20, -50, 50, 0],
-              }}
-            transition={{
-              duration: 10,
-              repeat: Infinity,
-              ease: "easeInOut",
-            }}
-            />
-             
-      <motion.div 
-        className="text-center mb-16"
-        initial={{ opacity: 0, y: 20 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        viewport={{ once: true }}
-        transition={{ duration: 0.5 }}
-      >
-        <span className="text-neutral-300 font-medium">Questions & Answers</span>
-        <h2 className="font-riffic text-4xl md:text-5xl font-bold py-2 mb-2 text-transparent bg-clip-text bg-gradient-to-r from-[#b4ff7f] to-[#ffdd88]">Frequently Asked Questions</h2>
-        <p className="text-muted-foreground max-w-2xl mx-auto">
-        Everything you need to know about finding your perfect coding partner
-        </p>
-      </motion.div>
+    <section id="faq" className="bg-[#0C0A09] py-20">
+      <div className="container relative mx-auto px-4 md:px-6 lg:px-8">
+        <motion.div
+          className="absolute left-10 right-20 h-44 w-44 rounded-full bg-gradient-to-r from-yellow-500/40 to-green-400/60 blur-[100px]"
+          animate={{
+            x: [0, 50, -20, 40, 0],
+            y: [0, -30, 20, -20, 0],
+            rotate: [0, 20, -50, 50, 0],
+          }}
+          transition={{
+            duration: 10,
+            repeat: Infinity,
+            ease: 'linear',
+          }}
+        />
 
-      <div className="max-w-3xl mx-auto  border bg-[#0C0A09] border-[#b4ff7f]/50 rounded-2xl shadow-lg overflow-hidden">
-        <div className="p-1 md:p-4">
-        {faqs.map((faq, index) => (
-          <FaqItem key={index} faq={faq} index={index} />
-        ))}
+        <motion.div
+          className="absolute bottom-10 right-10 h-52 w-52 rounded-full bg-gradient-to-r from-yellow-500/40 to-green-400/60 blur-[90px] md:right-20"
+          animate={{
+            x: [0, 10, -20, 10, 0],
+            y: [0, -10, 20, -20, 0],
+            rotate: [0, 20, -50, 50, 0],
+          }}
+          transition={{
+            duration: 10,
+            repeat: Infinity,
+            ease: 'easeInOut',
+          }}
+        />
+
+        <motion.div
+          className="mb-16 text-center"
+          initial={{ opacity: 0, y: 20 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: true }}
+          transition={{ duration: 0.5 }}>
+          <span className="font-medium text-neutral-300">
+            Questions & Answers
+          </span>
+          <h2 className="mb-2 bg-gradient-to-r from-[#b4ff7f] to-[#ffdd88] bg-clip-text py-2 font-riffic text-4xl font-bold text-transparent md:text-5xl">
+            Frequently Asked Questions
+          </h2>
+          <p className="mx-auto max-w-2xl text-muted-foreground">
+            Everything you need to know about finding your perfect coding
+            partner
+          </p>
+        </motion.div>
+
+        <div className="mx-auto max-w-3xl overflow-hidden rounded-2xl border border-[#b4ff7f]/50 bg-[#0C0A09] shadow-lg">
+          <div className="p-1 md:p-4">
+            {faqs.map((faq, index) => (
+              <FaqItem key={index} faq={faq} index={index} />
+            ))}
+          </div>
         </div>
-      </div>
 
-      <motion.div 
-        className="mt-12 text-center"
-        initial={{ opacity: 0 }}
-        whileInView={{ opacity: 1 }}
-        viewport={{ once: true }}
-        transition={{ duration: 0.5, delay: 0.3 }}
-      >
-        <p className="text-muted-foreground mb-4">Still have questions?</p>
-        <motion.button 
-        className="px-6 py-2 bg-neutral-700 text-[#b4ff7f] font-medium rounded-full hover:bg-[#b4ff7f] hover:text-black transition-all duration-300"
-        whileHover={{ scale: 1.05 }}
-        whileTap={{ scale: 0.95 }}
-        >
-        Contact Support
-        </motion.button>
-      </motion.div>
-    </div>
+        <motion.div
+          className="mt-12 text-center"
+          initial={{ opacity: 0 }}
+          whileInView={{ opacity: 1 }}
+          viewport={{ once: true }}
+          transition={{ duration: 0.5, delay: 0.3 }}>
+          <p className="mb-4 text-muted-foreground">Still have questions?</p>
+          <motion.button
+            onClick={() => window.open('mailto:codeesignspa@gmail.com','_blank')}
+            className="rounded-full bg-neutral-700 px-6 py-2 font-medium text-[#b4ff7f] transition-all duration-300 hover:bg-[#b4ff7f] hover:text-black"
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}>
+            Contact Support
+          </motion.button>
+        </motion.div>
+      </div>
     </section>
-  );
+  )
 };
 
 export default FAQSection;
