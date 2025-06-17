78722d4ac5b8e53b80f3c35988ab4d530954fdf7
2sandeep7 <ssandeep8696@gmail.com>
2025-06-17 12:55:00 +0530

    fix: sign in page resposive

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -11,14 +11,13 @@ export default function AuthPage() {
   }, []);
 
   return (
-    <div className="grid grid-cols-3 bg-gradient-to-br from-neutral-700 via-neutral-800 to-neutral-900">
-   <div className="">
-          <Signin />
-   
-   </div>
+    <div className="grid grid-cols-1 md:grid-cols-3 bg-gradient-to-br from-neutral-700 via-neutral-800 to-neutral-900 min-h-screen">
+      <div className="flex items-center justify-center p-4 md:p-0">
+        <Signin />
+      </div>
       <div 
-      style={{ backgroundImage: 'url("/sign-in-bg.png")', backgroundSize: 'cover', backgroundPosition: 'left' }}
-      className="hidden md:flex items-center justify-center col-span-2">     
+        style={{ backgroundImage: 'url("/sign-in-bg.png")', backgroundSize: 'cover', backgroundPosition: 'left' }}
+        className="hidden md:flex items-center justify-center col-span-2">     
       </div>
     </div>
   );
