88866e203572ce173c045827faf58e1036559597
2sandeep7 <ssandeep8696@gmail.com>
2025-06-30 01:22:28 +0530

    fix: create gitdate profile comp  ui

--- src/components/create-gitdate-profile.tsx
+++ src/components/create-gitdate-profile.tsx
@@ -1,31 +1,34 @@
-import { Github } from "lucide-react"
-import { Button } from "./ui/button"
-import { AsyncFunctionType } from "@/types/func"
-import { Card } from "./ui/card"
+import { Github } from 'lucide-react'
+import { Button } from './ui/button'
+import { AsyncFunctionType } from '@/types/func'
+import { Card } from './ui/card'
+import Image from 'next/image'
 
 export const CreateGitDateProfile = ({
-    handleCreateProfile
-}:{
-    handleCreateProfile : AsyncFunctionType
-}) =>{
-    return (
-         <div className="min-h-screen   flex items-center justify-center p-4">
-        <Card className=" rounded-3xl p-8  max-w-md w-full text-center">
-          <div className="w-16 h-16 rounded-2xl bg-gradient-to-br from-gray-900 to-gray-800 border border-gitdate flex items-center justify-center mx-auto mb-6 shadow-lg">
-            <Github className="w-8 h-8 text-gray-300" />
-          </div>
-          <h1 className="text-2xl font-bold text-slate-900 dark:text-white mb-2">
-            Create Your Profile
-          </h1>
-          <p className="text-slate-600 dark:text-slate-300 mb-6">
-            Get started by creating your GitHub profile in just one click.
-          </p>
-          <Button
-            onClick={handleCreateProfile}
-          >
-            Create Profile
-          </Button>
-        </Card>
-      </div>
-    )
+  handleCreateProfile,
+}: {
+  handleCreateProfile: AsyncFunctionType
+}) => {
+  return (
+    <div className="flex items-center justify-center p-4">
+      <Card className="w-full max-w-md rounded-3xl p-8 text-center border-none">
+        <div className="mx-auto mb-6 flex h-16 w-16 items-center justify-center rounded-2xl bg-gradient-to-br from-gray-900 to-neutral-800 shadow-[3px_4px_3px_rgba(0,0,0,0.1)] drop-shadow-[5px_5px_3px_rgba(255,255,255,0.2)]">
+          <Image
+            src={`/assets/logo/gitdate-dark-logo.svg`}
+            alt="gitdate-logo"
+            width={100}
+            height={100}
+            className="h-10 w-10"
+          />
+        </div>
+        <h1 className="mb-2 text-2xl font-bold text-slate-900 dark:text-white">
+          Create Your Profile
+        </h1>
+        <p className="mb-6 text-slate-600 dark:text-slate-300">
+          Get started by creating your GitHub profile in just one click.
+        </p>
+        <Button onClick={handleCreateProfile}>Create Profile</Button>
+      </Card>
+    </div>
+  )
 }
