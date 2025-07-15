dd38a1a29241a9b8098f1e74bf8b8e0e0d166f27
2sandeep7 <ssandeep8696@gmail.com>
2025-07-15 23:26:09 +0530

    feat: warning status

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -3,20 +3,24 @@ import { AppSidebar } from '@/components/app-sidebar'
 import { Menu } from 'lucide-react'
 import { ModeToggle } from '@/components/ui/mode-toggle'
 import { ShareGitDate } from '@/components/share-gitdate'
+import { AppStatusBanner } from '@/components/statusbanner'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
       <AppSidebar />
-      <main className="flex-1 p-4 md:p-6 md:pt-6">
-        <div className="flex items-center gap-5 pl-4 justify-between">
-          <div className="flex items-center gap-5">
-            <SidebarTrigger />
-            <ModeToggle />
+      <main className="flex-1">
+        <AppStatusBanner />
+        <div className="p-4 md:p-6 md:pt-6">
+          <div className="flex items-center justify-between gap-5 pl-4">
+            <div className="flex items-center gap-5">
+              <SidebarTrigger />
+              <ModeToggle />
+            </div>
+            <ShareGitDate />
           </div>
-          <ShareGitDate />
+          {children}
         </div>
-        {children}
       </main>
     </SidebarProvider>
   )
